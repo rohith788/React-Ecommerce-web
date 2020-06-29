daa13ffb76fb568cede548c73f7d4c902562f6ca
rohith788 <srohith788@gmail.com>
2020-06-29 10:04:48 -0400

    added collection item component with come Css changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components'
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App () {
     return (
       <div >
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     );
