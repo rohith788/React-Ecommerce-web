0541c45f24a911b5dc31e88697a50792b0aff672
rohith788 <srohith788@gmail.com>
2020-07-02 23:14:11 -0400

    Linked Firebase, Firestone and added signup component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 import './App.css';
 
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -21,9 +21,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user})
-      console.log(user.email)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+      const userRef = await createUserProfileDocument(userAuth)
+
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        }, () => console.log(this.state))
+      })
+      
+      }
+    this.setState({currentUser: userAuth})
     })
   }
 
