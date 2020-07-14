b8e0d548d8ae36b2167ca024de74c71a5ce0ab78
rohith788 <srohith788@gmail.com>
2020-07-13 20:08:59 -0400

    remove item, change item quantity functions added in check out page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,9 +6,15 @@ import {Link} from 'react-router-dom'
 
 import {auth} from '../../firebase/firebase.utils'
 
+import { createStructuredSelector } from 'reselect'
+
 import CartIcon from '../cart-icon/cart-icon.component'
 
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+
+import { selectCurrentUser } from '../../redux/user/user.selector'
 
 // import {ReachComponent as Logo} from '../../assets/crown.svg'
 
@@ -38,9 +44,9 @@ const Header = ({currentUser, hidden}) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden 
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
