8f37cc141ff1135c39cad29424eb104f94b83ebd
rohith788 <srohith788@gmail.com>
2020-07-14 20:43:18 -0400

    s

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
-import SHOP_DATA from './shop.data.js'
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.jsx'
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super()
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+import { selectCollections } from '../../redux/shop/shop.selector'
 
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'>
+const ShopPage = ({collections}) => (
+        <div className='shop-page'>
             {
             collections.map(({id, ...otherCollectionProps }) => (
             <CollectionPreview key={id} {...otherCollectionProps} />
             ))
             }
-        </div>);
-    }
-}
+        </div>
+)
 
-export default ShopPage
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage)
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
+
 import storage from 'redux-persist/lib/storage'
 
 import cartReducer from './cart/cart.reducer'
 import userReducer from './user/user.reducer'
+import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 
-const persistConfig = {
+const persistConfig  = {
     key: 'root',
     storage,
     whitelist: ['cart']
@@ -13,8 +16,12 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer, 
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
    }
 )
+
+
 
 export default persistReducer(persistConfig, rootReducer)
