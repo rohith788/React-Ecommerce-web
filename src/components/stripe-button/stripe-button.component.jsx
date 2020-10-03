286b056fafb383f7f230103faf1d3c0dc1203adb
rohith788 <srohith788@gmail.com>
2020-10-03 18:47:05 -0400

    try again

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout'
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100
-    const publishableKey = 'pk_test_51H5IVgBEP4zCtsz0LsANs3hsR1FxL8QhoDFQrAddZKtWWGhVbPyJjWEh45ybE91uMILqDYpLKdoHxdIPfry61ih700zjEPbhx8'
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "sk_test_51H5IVgBEP4zCtsz07lOBkHSGhxM3WOdpaCAmByhSRUaW2239DVFj81AWPVphxeUBh4XOFLspjHOO9JKUQRmER5mw0018gWYaU9";
 
-    const onToken = token => {
-        console.log(token)
-        alert('Payment Sucessful')
-    }
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Sucessful");
+  };
 
-    return (
-        <StripeCheckout 
-            lable='Pay Now'
-            name='SomeSite'
-            billingAddress
-            shippingAddress
-            description={`Your total is $${price}`}
-            amount={priceForStripe}
-            panellabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
-    )
-}
+  return (
+    <StripeCheckout
+      lable="Pay Now"
+      name="SomeSite"
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panellabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
 
-export default StripeCheckoutButton
+export default StripeCheckoutButton;
